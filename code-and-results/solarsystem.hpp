808a23da6eb4490dce5d6e048e1e68073cea5c01
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-10-02 12:02:37 +0200

    initial class structur

--- code-and-results/solarsystem.hpp
+++ code-and-results/solarsystem.hpp
@@ -15,10 +15,20 @@ class SolarSystem {
 
 private:
 
+
 protected:
+  string m_name
+  vec m_x,_y,m_m_z
+  m_force
 
 
 
+public:
+  void initialize(m_name)   // Use keys for each planet
+  void grav_force(m_name1,m_)         // calculate force between stellar objects
+  void Verlet()             // Verlet solver
+  void EulerChromer()       // EulerChromer solver
+  void RungeKutta4()        // RungeKutta4 solver
 
 
 };
