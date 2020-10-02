f7b6585e4ec6f486d1897399fc55704517657cd7
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-10-02 11:30:41 +0200

    Create solarsystem.hpp

---
+++ code-and-results/solarsystem.hpp
@@ -0,0 +1,28 @@
+#ifndef SolarSystem_HPP
+#define SolarSystem_HPP
+
+#include <fstream>
+#include <armadillo>
+#include <chrono>
+
+using namespace std;
+using namespace arma;
+using namespace chrono;
+
+// Setting up solarsystem as a superclass
+
+class SolarSystem {
+
+private:
+
+protected:
+
+
+
+
+
+};
+
+
+
+#endif
