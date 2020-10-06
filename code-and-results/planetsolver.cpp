f3aa8e85739146d48f4d110b3cd53a564c2a1496
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-10-06 21:46:24 +0200

    fixed vector <string> with index [] (std style indexing)

--- code-and-results/planetsolver.cpp
+++ code-and-results/planetsolver.cpp
@@ -2,7 +2,7 @@
 #include "planets.hpp"
 
 void PlanetSolver::init(vector<string> names){
-  vector<string> m_names = names;
+  vector<string> m_names = names; // std type vector
 
   //to be used in all derived classes
   vec masses = vec(m_N);
@@ -10,7 +10,7 @@ void PlanetSolver::init(vector<string> names){
   vec params = vec(2);
   Planets Planet;
   for (int i = 0; i < m_N; ++i){
-    params = Planet.initialize(m_names(i));
+    params = Planet.initialize(m_names[i]);
     masses(i) = params(0);
     distances(i) = params(1);
   }
