8f80934cc40aa36958caac86afe874c04cc2e482
Isak Aaby <isakaaby@Isaks-MacBook-Pro.local>
2020-10-08 22:14:03 +0200

    good

--- code-and-results/planets.cpp
+++ code-and-results/planets.cpp
@@ -8,35 +8,6 @@ using namespace std;
 using namespace chrono;
 using namespace arma;
 
-<<<<<<< HEAD
-
-double m_sun = 2E+30;
-double m_earth = (6E+24)/m_sun;
-double m_jupiter = (1.9E+27)/m_sun;
-double m_mars = (6.6E23)/m_sun;
-double m_venus = (4.9E24)/m_sun;
-double m_saturn = (5.5E26)/m_sun;
-double m_mercury = (3.3E23)/m_sun;
-double m_uranus = (8.8E25)/m_sun;
-double m_neptune = (1.03E26)/m_sun;
-double m_pluto = (1.31E22)/m_sun;
-
-//Defining distances from sun for the different planets in AU
-double r_earth = 1;
-double r_jupiter = 5.20;
-double r_mars = 1.52;
-double r_venus = 0.72;
-double r_saturn = 9.54;
-double r_mercury = 0.39;
-double r_uranus = 19.19;
-double r_neptune = 30.065;
-double r_pluto = 39.53;
-
-double mass;
-double distance_sun;
-double sun = 1;
-=======
->>>>>>> 88476de5d03e874f28b3a9bb1f5cbeca349ade23
 
 double sun = 1.;
 
@@ -87,11 +58,6 @@ vec Planets::initialize(string planet){
   double value = atof(names.cstr());
   //cout << names << "\n";
   return value;
-<<<<<<< HEAD
->>>>>>> ff47c7ac2e14fce3b1ba7362944ca93824ac2612
-
-=======
->>>>>>> 88476de5d03e874f28b3a9bb1f5cbeca349ade23
 }
 */
 
