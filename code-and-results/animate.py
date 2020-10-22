c8204712b4078762dc0d876e8941bb6e49f7c4bf
Lasse Steinnes <lasse.steinnes93@gmail.com>
2020-10-22 21:57:10 +0200

    Animation works :)

--- code-and-results/animate.py
+++ code-and-results/animate.py
@@ -1,9 +1,9 @@
-"""
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.axes3d as p3
+from matplotlib.animation import FuncAnimation
 
-N = int(input("Insert number of objects N: "))
+# N = int(input("Insert number of objects N: ")) Could choose to use this
 
 infile1 = open("./results/position_x.txt", "r")
 infile2 = open("./results/position_y.txt", "r")
@@ -14,48 +14,62 @@ y = np.loadtxt(infile2)
 z = np.loadtxt(infile3)
 planets = np.genfromtxt("./results/planet_names.txt",dtype='str')
 
+pos =  np.array([x[:,0],y[:,0],z[:,0]]); pos1 = np.array([x[:,1],y[:,1],z[:,1]])
+pos2 = np.array([x[:,2],y[:,2],z[:,2]]); pos3 = np.array([x[:,3],y[:,3],z[:,3]])
+pos4 = np.array([x[:,4],y[:,4],z[:,4]]); pos5 = np.array([x[:,5],y[:,5],z[:,5]])
+pos6 = np.array([x[:,6],y[:,6],z[:,6]]); pos7 = np.array([x[:,7],y[:,7],z[:,7]])
+pos8 = np.array([x[:,8],y[:,8],z[:,8]]); pos9 = np.array([x[:,9],y[:,9],z[:,9]])
+
+# initiate figure
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-#ax.set_facecolor("white")
-#ax.set_axis_off()
+ax = p3.Axes3D(fig)
 
-## chose colour of axis
-ax.tick_params(axis='x',colors='grey')
-ax.tick_params(axis='y',colors='grey')
-ax.tick_params(axis='z',colors='grey')
+# Setting the axes properties so they are constant for each frame
+ax.set_xlim3d([-30.0, 40.0])
+ax.set_xlabel('X [AU]')
 
-ax.w_xaxis.line.set_color("grey")
-ax.w_yaxis.line.set_color("grey")
-ax.w_zaxis.line.set_color("grey")
+ax.set_ylim3d([-30.0, 40.0])
+ax.set_ylabel('Y [AU]')
 
-# make the panes transparent
-#ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-#ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-#ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-
-# make the grid lines transparent
-ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,0)
-ax.yaxis._axinfo["grid"]['color'] =  (1,1,1,0)
-ax.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
-
-if x[0,0] == 0.0:
-    ax.plot(x[:,0], y[:,0], ".", color = "orange", label = "Sun")
-    for i in range(1, N):
-        ax.plot(x[:,i], y[:,i], z[:,i] , label = planets[i])
-        ax.set_xlabel('x [AU]',color = "grey")
-        ax.set_ylabel('y [AU]',color = "grey")
-        ax.set_zlabel('z [AU]',color = "grey")
-        plt.legend()
-    plt.show()
-
-else:
-    for i in range(0, N):
-        ax.plot(x[:,i], y[:,i], z[:,i], label = planets[i])
-        ax.set_xlabel('x [AU]',color = "grey")
-        ax.set_ylabel('y [AU]',color = "grey")
-        ax.set_zlabel('z [AU]',color = "grey")
-        plt.legend()
-    plt.show()
+ax.set_zlim3d([-15.0, 10.0])
+ax.set_zlabel('Z [AU]')
 
 """
-# Take inspiration from what is above to animate in 3D
+#line.set_data(data[:2, :num])
+#line.set_3d_properties(data[2, :num])
+"""
+
+# This is the first frame, and it is a point in 3D for each planet
+line, = ax.plot(pos[0,0:1],pos[1,0:1],pos[2,0:1])
+line1, = ax.plot(pos1[0,0:1],pos1[1,0:1],pos1[2,0:1])
+line2, = ax.plot(pos2[0,0:1],pos2[1,0:1],pos2[2,0:1])
+line3, = ax.plot(pos3[0,0:1],pos3[1,0:1],pos3[2,0:1])
+line4, = ax.plot(pos4[0,0:1],pos4[1,0:1],pos4[2,0:1])
+line5, = ax.plot(pos5[0,0:1],pos5[1,0:1],pos5[2,0:1])
+line6, = ax.plot(pos6[0,0:1],pos6[1,0:1],pos6[2,0:1])
+line7, = ax.plot(pos7[0,0:1],pos7[1,0:1],pos7[2,0:1])
+line8, = ax.plot(pos8[0,0:1],pos8[1,0:1],pos8[2,0:1])
+line9, = ax.plot(pos9[0,0:1],pos9[1,0:1],pos9[2,0:1])
+
+def animation_frame(num):
+    # Update lines  with x,y and z data
+    line.set_data(pos[:2, :num])         # x,y axis
+    line.set_3d_properties(pos[2, :num]) # z-axis
+    line1.set_data(pos1[:2, :num]); line1.set_3d_properties(pos1[2, :num])
+    line2.set_data(pos2[:2, :num]); line2.set_3d_properties(pos2[2, :num])
+    line3.set_data(pos3[:2, :num]); line3.set_3d_properties(pos3[2, :num])
+    line4.set_data(pos4[:2, :num]); line4.set_3d_properties(pos4[2, :num])
+    line5.set_data(pos5[:2, :num]); line5.set_3d_properties(pos5[2, :num])
+    line6.set_data(pos6[:2, :num]); line6.set_3d_properties(pos6[2, :num])
+    line7.set_data(pos7[:2, :num]); line7.set_3d_properties(pos7[2, :num])
+    line8.set_data(pos8[:2, :num]); line8.set_3d_properties(pos8[2, :num])
+    line9.set_data(pos9[:2, :num]); line9.set_3d_properties(pos9[2, :num])
+    return [line,line1,line2,line3,line4,line5,line6,line7,line8,line9]
+
+N_points = len(x[:,1]);
+
+#ani = animation.FuncAnimation(fig, func = animation_frame, N, fargs=(data, line), interval=10000/N, blit=False)
+
+animation = FuncAnimation(fig, func = animation_frame, frames = range(N_points),interval = 150)#, #blit=True)
+#animation.save('matplot003.gif', writer='imagemagick')
+plt.show()
